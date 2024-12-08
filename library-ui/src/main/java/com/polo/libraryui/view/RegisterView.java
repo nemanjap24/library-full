39a5b090901095f019f396ccc04c6e79b2e84048
Liavo420 <martin.ljavo@gmail.com>
2024-12-08 17:07:04 +0100

    fix: styles.css and UI layout

--- library-ui/src/main/java/com/polo/libraryui/view/RegisterView.java
+++ library-ui/src/main/java/com/polo/libraryui/view/RegisterView.java
@@ -3,6 +3,7 @@ package com.polo.libraryui.view;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -22,37 +23,38 @@ public class RegisterView {
         root.setAlignment(Pos.CENTER);
         root.getStyleClass().add("form-container");
 
+
         GridPane formGrid = new GridPane();
         formGrid.setVgap(10);
         formGrid.setHgap(10);
         formGrid.setAlignment(Pos.CENTER);
 
         usernameField = new TextField();
-        usernameField.setPromptText("Username");
 
         emailField = new TextField();
-        emailField.setPromptText("Email");
 
         passwordField = new PasswordField();
-        passwordField.setPromptText("Password");
 
         confirmPasswordField = new PasswordField();
-        confirmPasswordField.setPromptText("Confirm Password");
 
-        formGrid.add(new TextField("Username:"), 0, 0);
+        formGrid.add(new Label("Username:"), 0, 0);
         formGrid.add(usernameField, 1, 0);
-        formGrid.add(new TextField("Email:"), 0, 1);
+        formGrid.add(new Label("Email:"), 0, 1);
         formGrid.add(emailField, 1, 1);
-        formGrid.add(new TextField("Password:"), 0, 2);
+        formGrid.add(new Label("Password:"), 0, 2);
         formGrid.add(passwordField, 1, 2);
-        formGrid.add(new TextField("Confirm Password:"), 0, 3);
+        formGrid.add(new Label("Confirm Password:"), 0, 3);
         formGrid.add(confirmPasswordField, 1, 3);
 
         registerButton = new Button("Register");
         backButton = new Button("Back");
         backButton.getStyleClass().add("back-button");
 
+
         root.getChildren().addAll(formGrid, registerButton, backButton);
+        //root.getStylesheets().add(getClass().getResource("library-ui/src/main/resources/com/polo/libraryui/styles.css").toExternalForm());
+
+
     }
 
     public Parent getView() { return root; }
